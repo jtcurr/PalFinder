c23158ec8416573886b716dae602ffe7711080bc
Jonathan <jonathan.eleiott@gmail.com>
2017-02-07 16:18:28 -0800

    (feat) Update Twilio number works in profile

--- client/scripts/controllers/addTwilioNumber.js
+++ client/scripts/controllers/addTwilioNumber.js
@@ -1,7 +1,16 @@
-angular.module('myApp').controller('addTwilioNumber', function($rootScope, $scope) {
+angular.module('myApp').controller('addTwilioNumber', function($rootScope, $scope, databaseAndAuth) {
   $scope.updateTwilioNumber = function() {
     var user = firebase.auth().currentUser;
     console.log('user in updateTwilioNumber', user);
     var newTwilioNumber = $scope.newTwilioNumber;
+    databaseAndAuth.database.ref('users/' + user.uid).update({
+      "twilioNumber": newTwilioNumber
+    }, function(error) {
+      if (error) {
+        console.log("Twilio number could not be saved." + error);
+      } else {
+        console.log("Twilio number saved successfully.");
+      }
+    });
   }
 });
