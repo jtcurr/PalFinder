343b69cf2110b11bb1a05287dd191622ee1758a9
Ahmed Zoghayyer <azzoghayyer@gmail.com>
2017-02-08 10:58:10 -0800

    (add) twilio account, and set it up to recevie message and response with an XML response

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var https = require('https');
+var bodyParser = require('body-parser');
 var app = express();
 var fs = require('fs');
 var auth = require('./authorization.js');
@@ -25,21 +26,38 @@ var options = {
 // app.use('/styles', express.static(path.join(__dirname, '/../client/styles')));
 // app.use('/partials', express.static(path.join(__dirname, '/../client/partials')));
 // app.use('/images', express.static(path.join(__dirname, '/../client/images')));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname + '/../client'));
+
+// app.get('/message', function(req, res) {
+// 	res.send('HHIIHIH');
+// });
+app.post('/message', function(req, res) {
+  console.log('----------', req.body);
+  var msgFrom = req.body.From;
+  var msgBody = req.body.Body;
+  res.send(`<Response>
+    <Message>
+    Hello ${msgFrom}. You said: ${msgBody}
+    </Message>
+    </Response>`)
+});
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '/../client/index.html'));
 });
-
 
 // app.get('/send-message', function(req,res) {
 //   console.log('serving request ' + req.method + ' at ' + req.url);
 //   res.sendFile(path.join(__dirname, '/../client/index.html'));
 // });
 
-var server = https.createServer(options, app);
+// var server = https.createServer(options, app);
 
-server.listen(3000, function () {
-  console.log('Server listening on port 3000!')
+// server.listen(3000, function () {
+//   console.log('Server listening on port 3000!')
+// })
+app.listen(3000, function() {
+  console.log('You are listening on port 3000!')
 })
