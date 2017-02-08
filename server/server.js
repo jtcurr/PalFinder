c50cfb54f81c95dad4c0d573a99f3070b157c626
Jonathan <jonathan.eleiott@gmail.com>
2017-02-08 11:36:19 -0800

    Fix merge conflicts

--- server/server.js
+++ server/server.js
@@ -6,6 +6,20 @@ var fs = require('fs');
 var auth = require('./authorization.js');
 var watson = require('watson-developer-cloud');
 var recorder = require('./recorder')
+var bodyParser = require('body-parser');
+var twilio = require('twilio');
+
+// Twilio Credentials
+var accountSid = 'AC4d28a323f87518fb7415f19864d01c56';
+var authToken = 'b0bd854524b33b38956f4ea4804a46e5';
+
+//require the Twilio module and create a REST client
+var client = require('twilio')(accountSid, authToken);
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+>>>>>>> Add heroku
 
 // var text_to_speech = watson.text_to_speech(auth.text_to_speech);
 // var params = {
@@ -32,10 +46,21 @@ app.use(express.static(__dirname + '/../client'));
 //start and stop recording
 app.get('/recorder', recorder.toggleState);
 
+app.post('/voice', function(req, res) {
+  console.log('inside the voice post');
+  var twimlRes = new twilio.TwimlResponse();
+
+  twimlRes.message('Thank you for sending me a message');
+  res.writeHead(200, {
+    'Content-Type':'text/xml'
+  });
+  res.end(twimlRes.toString());
+})
+>>>>>>> Add heroku
+
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '/../client/index.html'));
 });
-
 
 // app.get('/send-message', function(req,res) {
 //   console.log('serving request ' + req.method + ' at ' + req.url);
